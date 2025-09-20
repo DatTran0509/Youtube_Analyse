24fbf6df21177e3a4612d8e700aa4477ebbd149c
DatTran0509 <dat881070@gmail.com>
2025-09-21 02:03:12 +0700

    update docker

---
+++ client/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  outputFileTracingRoot: __dirname, // Explicitly set the root directory for output tracing
+};
+
+module.exports = nextConfig;
